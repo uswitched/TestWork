86b19da6895e16810adda1cd2efda08e35ba72d2
uswitch <usflua@gmail.com>
2021-03-20 10:51:54 +0200

    Add Java Models class from JSON for Gson Add Glide, Retrofit, Hilt, RxJava Library

---
+++ app/src/main/java/ua/netronic/testwork/MainActivity.java
@@ -0,0 +1,14 @@
+package ua.netronic.testwork;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
